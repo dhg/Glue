2a6e0321d31e3dbdfc61492f4574111482781c3b
David Gamache <dhgamache@tw-mbp13-dgamache.local>
2012-04-05 08:43:21 -0700

    adding the initial pass at processing data

--- js/glue.js
+++ js/glue.js
@@ -18,7 +18,7 @@ $(function() {
 
   // Glue includes -----------------------------------
   includes.each(function() {
-    renderTemplate($(this).attr('glue-src'), $(this));
+    renderTemplate($(this).attr('glue-src'), $(this), $(this).attr('glue-data'));
   })
 
   // Handle links ------------------------------------
@@ -31,29 +31,46 @@ $(function() {
     return location.hash.substr(1);
   }
 
-  function routeToTemplate(templateName, templateContainer) {
+  function routeToTemplate(templateName, templateContainer, templateData) {
     $('.rendered').removeClass('rendered');
     templateContainer = templateContainer || defaultYield;
-    renderTemplate(templateName, templateContainer);
+    renderTemplate(templateName, templateContainer, templateData);
     location.hash = "#" + templateName;
   }
 
-  function renderTemplate(templateName, templateContainer) {
+  function renderTemplate(templateName, templateContainer, templateData) {
     $.ajax({
       url: templateName + ".html",
       cache: false
     }).done(function(html) {
       templateContainer
         .empty()
-        .append(html)
+        .append(html);
+      injectData(templateContainer, templateData)
       var subincludes = templateContainer.find($('[glue-src]'));
       if(subincludes.length != 0) {
         subincludes.each(function() {
-          renderTemplate($(this).attr('glue-src'), $(this));
+          renderTemplate($(this).attr('glue-src'), $(this), $(this).attr('glue-data'));
         })
       } else {
         defaultYield.addClass('rendered');
       }
     });
   }
+
+  function injectData(templateContainer, templateData) {
+    $.ajax({
+      url: templateData + ".json",
+      cache: false
+    }).done(function(json) {
+      console.log(json)
+      templateContainer.find('[glue-value]').each(function() {
+        var jsonKey = $(this).attr('glue-value').toString();
+        if(json[jsonKey]) {
+          $(this).html(json[jsonKey])
+        }
+      });
+    })
+  }
+
 });
